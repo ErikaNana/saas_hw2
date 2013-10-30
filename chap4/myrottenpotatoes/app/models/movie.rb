fc5a9af5a270d7c4a8f8e29585e184a64836035d
Erika Nana <enana@hawaii.edu>
2013-10-30 15:43:47 -0700

    remove chap 4

--- chap4/myrottenpotatoes/app/models/movie.rb
+++
@@ -1,6 +0,0 @@
-class Movie < ActiveRecord::Base #thing that goes in the database
-    attr_accessible :title, :rating, :release_date, :tags_attributes
-    def self.all_ratings
-        Array['G', 'PG', 'PG-13', 'R', 'NC-17']
-    end
-end
